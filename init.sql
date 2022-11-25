197c9c9276dfd8d23a1caeb9d8f4691a57c47cb4
Artur <artur@192-168-10-239.local.balticum.lt>
2022-11-25 23:57:44 +0200

    Task 8

--- init.sql
+++ init.sql
@@ -11,5 +11,15 @@ count int
 );
 
 insert into carts default values;
-
 insert into cart_items (cart_id, product_id, count) values ((select id from carts limit 1), gen_random_uuid(), 197);
+
+create table if not exists orders (
+id uuid,
+user_id uuid,
+cart_id uuid references carts(id),
+payment varchar(500),
+delivery varchar(500),
+comments varchar(500),
+status varchar(500),
+total int
+);
